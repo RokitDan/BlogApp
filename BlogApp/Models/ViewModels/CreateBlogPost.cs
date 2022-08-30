2868f19155371bc6fb97517c1f28075d4a9372aa
RokitDan <dleeschell@gmail.com>
2022-08-30 09:40:20 -0400

    Latest

---
+++ BlogApp/Models/ViewModels/CreateBlogPost.cs
@@ -0,0 +1,10 @@
+﻿namespace BlogApp.Models.ViewModels
+{
+    public class CreateBlogPost
+    {
+        public BlogPost BlogPost { get; set; } = new();
+        public List<Tag> Tags { get; set; } = new();
+        public List<Category> Categories { get; set; } = new();
+
+    }
+}
