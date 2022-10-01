aa79b6811fe076627b3aa31de67249eb7acd7c97
RokitDan <dleeschell@gmail.com>
2022-10-01 16:40:15 -0400

    Prepared for Railway

---
+++ Helpers/DataHelper.cs
@@ -0,0 +1,20 @@
+﻿using BlogApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogApp.Helpers
+{
+    public static class DataHelper
+    {
+
+        public static async Task ManageDataAsync(IServiceProvider svcProvider)
+        {
+            //Service: An instance of db context
+            var dbContextSvc = svcProvider.GetRequiredService<ApplicationDbContext>();
+
+            //Migration: This is the programmatic equivalent to Update-Database
+            await dbContextSvc.Database.MigrateAsync();
+        }
+
+
+    }
+}
